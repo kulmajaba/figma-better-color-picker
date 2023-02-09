a48fee07acf03790a8c82184d6e4280ea49b96b2
Mika Kuitunen <mika.kuitunen@etasku.fi>
2023-02-09 13:56:35 +0200

    New public asset folder and updated build configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './src',
+  publicDir: 'assets/public',
   plugins: [react()],
   build: {
     target: 'esnext',
