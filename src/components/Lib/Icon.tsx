403e5afe6335df26f66f910ff0148ea8bfa18d86
Mika Kuitunen <mika.kuitunen@etasku.fi>
2023-10-23 17:10:55 +0300

    Fix type safety in RecursivePath

--- src/components/Lib/Icon.tsx
+++ src/components/Lib/Icon.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { isArray } from '../../types';
+
 /* Example
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="black"/>
@@ -8,7 +10,8 @@ import { FC } from 'react';
 
 type PathType = string | React.SVGProps<SVGPathElement>;
 
-// TODO: Type this as an object of PathType | PathType[] but keep the keyof declaration as accurate as possible
+// TODO: Type this as an object of PathType | readonly PathType[] but keep the keyof declaration as accurate as possible
+// As const not needed if this typing ins successful
 const icons = {
   eyedropper:
     'M17.6594 5.41L18.5794 6.33L15.8894 9.02L14.9694 8.1L17.6594 5.41ZM17.6694 3C17.4094 3 17.1594 3.1 16.9594 3.29L13.8394 6.41L11.9094 4.5L10.4994 5.91L11.9194 7.33L2.99939 16.25V21H7.74939L16.6694 12.08L18.0894 13.5L19.4994 12.09L17.5794 10.17L20.6994 7.05C21.0994 6.65 21.0994 6.02 20.7094 5.63L18.3694 3.29C18.1694 3.1 17.9194 3 17.6694 3ZM6.91939 19L4.99939 17.08L13.0594 9.02L14.9794 10.94L6.91939 19Z',
@@ -44,7 +47,7 @@ interface Props {
 }
 
 const RecursivePath: FC<{ children: PathType | readonly PathType[] }> = ({ children }) =>
-  Array.isArray(children) ? (
+  isArray(children) ? (
     <>
       {children.map((child, index) => (
         <RecursivePath key={index}>{child}</RecursivePath>
@@ -53,7 +56,7 @@ const RecursivePath: FC<{ children: PathType | readonly PathType[] }> = ({ child
   ) : typeof children === 'string' ? (
     <path d={children} />
   ) : (
-    <path {...(children as React.SVGProps<SVGPathElement>)} />
+    <path {...children} />
   );
 
 const Icon: FC<Props> = ({ icon, className, rotateDeg }) => (
--- src/types.ts
+++ src/types.ts
@@ -78,4 +78,8 @@ export type SetEditingColorCallback = (
 
 export const isMouseEvent = (e: MouseEvent | TouchEvent): e is MouseEvent => e.type.includes('mouse');
 
+// https://github.com/microsoft/TypeScript/issues/17002#issuecomment-1529056512
+type ArrayType<T> = Extract<true extends T & false ? unknown[] : T extends readonly unknown[] ? T : unknown[], T>;
+export const isArray = Array.isArray as <T>(arg: T) => arg is ArrayType<T>;
+
 export const strictObjectKeys = Object.keys as <T extends Record<string, unknown>>(obj: T) => Array<keyof T>;
