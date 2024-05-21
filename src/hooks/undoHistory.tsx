90e89d06cbd48934e279f5537df5439433089591
Mika Kuitunen <mika.kuitunen@etasku.fi>
2024-05-21 14:44:58 +0300

    Add keyboard listener for undo

--- src/hooks/undoHistory.tsx
+++ src/hooks/undoHistory.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, useCallback, useContext, useMemo, useState } from 'react';
+import { createContext, FC, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 
 import { historySize } from '../constants';
 
@@ -20,22 +20,91 @@ export interface UndoHistory {
 
 interface HistoryContext extends UndoHistory {
   commitHistory: (colors: RowColor[], contrastColors: Color[], colorSpaceName: ColorSpaceName) => void;
-  undo: () => void;
-  redo: () => void;
 }
 
 const HistoryContext = createContext<HistoryContext>({
   history: [],
   historyIndex: -1,
   historyState: undefined,
-  commitHistory: () => undefined,
-  undo: () => undefined,
-  redo: () => undefined
+  commitHistory: () => undefined
 });
 
 export const HistoryProvider: FC<{ children?: React.ReactNode }> = ({ children }) => {
   const [history, setHistory] = useState<HistoryEntry[]>([]);
   const [historyIndex, setHistoryIndex] = useState(-1);
+  const [undoModifierDown, setUndoModifierDown] = useState(false);
+  const [redoModifierDown, setRedoModifierDown] = useState(false);
+
+  const undo = useCallback(() => {
+    if (historyIndex > 0) {
+      setHistoryIndex(historyIndex - 1);
+    }
+  }, [historyIndex]);
+
+  const redo = useCallback(() => {
+    if (historyIndex < history.length - 1) {
+      setHistoryIndex(historyIndex + 1);
+    }
+  }, [historyIndex, history]);
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      console.log('keydown', e.code);
+      switch (e.key) {
+        case 'Meta':
+          setUndoModifierDown(true);
+          break;
+        case 'Control':
+          setUndoModifierDown(true);
+          break;
+        case 'Shift':
+          setRedoModifierDown(true);
+          break;
+        default:
+          break;
+      }
+
+      if (e.code === 'KeyZ' && undoModifierDown && !redoModifierDown) {
+        undo();
+      } else if (e.code === 'KeyZ' && undoModifierDown && redoModifierDown) {
+        redo();
+      }
+    },
+    [undoModifierDown, redoModifierDown, undo, redo]
+  );
+
+  const onKeyUp = useCallback((e: KeyboardEvent) => {
+    console.log('keyup', e.code);
+    switch (e.key) {
+      case 'Meta':
+        setUndoModifierDown(false);
+        break;
+      case 'Control':
+        setUndoModifierDown(false);
+        break;
+      case 'Shift':
+        setRedoModifierDown(false);
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [onKeyDown]);
+
+  useEffect(() => {
+    window.addEventListener('keyup', onKeyUp);
+
+    return () => {
+      window.removeEventListener('keyup', onKeyUp);
+    };
+  }, [onKeyUp]);
 
   const commitHistory = useCallback(
     (colors: RowColor[], contrastColors: Color[], colorSpaceName: ColorSpaceName) => {
@@ -50,28 +119,14 @@ export const HistoryProvider: FC<{ children?: React.ReactNode }> = ({ children }
     [history, historyIndex]
   );
 
-  const undo = useCallback(() => {
-    if (historyIndex > 0) {
-      setHistoryIndex(historyIndex - 1);
-    }
-  }, [historyIndex]);
-
-  const redo = useCallback(() => {
-    if (historyIndex < history.length - 1) {
-      setHistoryIndex(historyIndex + 1);
-    }
-  }, [historyIndex, history]);
-
   const contextValue = useMemo(
     () => ({
       history,
       historyIndex,
       historyState: historyIndex !== -1 ? history[historyIndex] : undefined,
-      commitHistory,
-      undo,
-      redo
+      commitHistory
     }),
-    [history, historyIndex, commitHistory, undo, redo]
+    [history, historyIndex, commitHistory]
   );
 
   return <HistoryContext.Provider value={contextValue}>{children}</HistoryContext.Provider>;
